0b91ae7357b42b5c248376bfb3155fa9962ab2ba
BryantCR <bryancvargas20@gmail.com>
2021-09-12 15:16:28 -0600

    Core Assingment, all working as expected

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function likealerts(buttonlike){
+    console.log("This page says Ninja was liked");
+}
+
+function loginLogout(login){
+    login.innerText = "Logout";
+}
+
+function hide(buttonremove){
+    buttonremove.remove();
+}
